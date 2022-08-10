e00103d254d5924e6fafdd180b160990f04c8998
Ricardo Martins <ricardocmartins91@gmail.com>
2022-08-10 23:55:23 +0100

    788

--- src/main/scala/euler/prob701_800/Prob788.scala
+++ src/main/scala/euler/prob701_800/Prob788.scala
@@ -7,41 +7,21 @@ import euler.traits.UtilResult
 object Prob788 extends UtilResult {
 
   def calc: Long = {
-
-    // D(1)  = 9
-    // D(2)  = 18
-    // D(3)  = 270
-    // D(4)  = 603
-    // D(5)  = 8307
-    // D(6)  = 19737
-    // D(7)  = 265257
-    // D(10) = 21893256
-
-//    def bruteForce(n: Int): Boolean = {
-//      val nStr = n.toString
-//      val list: List[(Char, String)] = nStr.groupBy(identity).toList.sortBy(-_._2.length)
-//      list match {
-//        case first :: _ if first._2.length > nStr.length / 2 =>
-//          true
-//        case _ =>
-//          false
-//      }
-//    }
-
     val targetN = 2022
 
     val module = 1000000007
     val moduleBigInt = BigInt(module)
 
     val cacheFactorial: IndexedSeq[BigInt] =
-      (1 to targetN).scanLeft(BigInt(1)) { case (big, n) => (big * n).mod(moduleBigInt) }
+      (1 to targetN).scanLeft(BigInt(1)) { case (big, n) => big * n }
 
     val nineExp: IndexedSeq[BigInt] =
       (0 to targetN).map(BigInt(9).modPow(_, moduleBigInt))
 
-    def comb(n: Int, r: Int): BigInt =
+    def comb(n: Int, r: Int): BigInt = {
       (cacheFactorial(n) / (cacheFactorial(r) * cacheFactorial(n - r)))
         .mod(moduleBigInt)
+    }
 
     def calc(size: Int): BigInt =
       if (size <= 2)
@@ -62,6 +42,6 @@ object Prob788 extends UtilResult {
 
     D(targetN).mod(moduleBigInt).toLong
 
-    // Wrong: 259013341
+    // answer: 471745499
   }
 }
